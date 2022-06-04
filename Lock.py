4de7e2b6f2a5ccc8b94422f7cedeca73647bc871
DiscordHacks <106889505+JustAProDev@users.noreply.github.com>
2022-06-04 23:33:47 +0200

    Update Lock.py

--- Lock.py
+++ Lock.py
@@ -7,49 +7,49 @@ from discord.utils import get
 
 token = "yourtokenhere"
 
-Tokon = commands.Bot(description='Yurei', command_prefix=';', self_bot=True)
+DiscordHacks = commands.Bot(description='Yurei', command_prefix=';', self_bot=True)
 
-Tokon.lockgc = []
-Tokon.headers = {
+DiscordHacks.lockgc = []
+DiscordHacks.headers = {
     'authorization': token,
 }
 
-@Tokon.event
+@DiscordHacks.event
 async def lockloop():
     while True:
-        if not Tokon.lockgc == []:
-            for gcid in Tokon.lockgc:
-                response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=Tokon.headers)
+        if not DiscordHacks.lockgc == []:
+            for gcid in DiscordHacks.lockgc:
+                response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=DiscordHacks.headers)
                 if response.status_code == 429:
                     response_c = response.text
                     response_content = json.loads(response_c)
                     lockedsec = response_content.get('retry_after')
                 else:
                     headers = {"Authorization": token}
-                    payload = {"content": "__**successfully Locked GC Master Yurei !**__", "nonce": gcid}
+                    payload = {"content": "__**successfully Locked GC Master !**__", "nonce": gcid}
                     response = requests.post(url=f'https://discord.com/api/v9/channels/{gcid}/messages', headers=headers, json=payload)
                     for i in range(30):
-                        response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=Tokon.headers)
+                        response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=DiscordHacks.headers)
         await asyncio.sleep(0.8)
 
-@Tokon.event
+@DiscordHacks.event
 async def on_connect():
   print("Connected Master !")
   await lockloop()
 
-@Tokon.command()
+@DiscordHacks.command()
 async def lock(ctx, groupid):
-    Tokon.lockgc.extend([groupid] * 1)
+    DiscordHacks.lockgc.extend([groupid] * 1)
 
 
-@Tokon.command()
+@DiscordHacks.command()
 async def unlock(ctx, groupid=None):
     await ctx.send('__**Unlocked the Group Master !**__')
     if groupid:
-        Tokon.lockgc.pop(groupid)
-        print(f'Unlocking {groupid} in 120 seconds Master Yurei !')
+        DiscordHacks.lockgc.pop(groupid)
+        print(f'Unlocking {groupid} in 120 seconds Master !')
     else:
-        Tokon.lockgc.clear()
+        DiscordHacks.lockgc.clear()
         print(f'Unlocked All Groups')
 
-Tokon.run(token, bot=False, reconnect=True)
+DiscordHacks.run(token, bot=False, reconnect=True)
